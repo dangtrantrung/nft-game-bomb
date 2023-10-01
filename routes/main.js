41b77abdb35a9174e9ab373c704b3a31d575eea4
dangtrantrung <trung280584@gmail.com>
2023-10-01 12:38:09 +0700

    server web3 started

--- routes/main.js
+++ routes/main.js
@@ -1,19 +1,19 @@
 const Web3 = require('web3');
-const urlRPC=""; // your url rpc blockchain network
+const urlRPC = ""; // your url rpc blockchain network
 const web3 = new Web3(urlRPC);
 
-module.exports = function(app){
+module.exports = function(app) {
 
-    app.get("/", function(req, res){
+    app.get("/", function(req, res) {
         res.render("login");
     });
 
-    app.post("/verifyAccount", function(req, res){
-        if(!req.body.hashString || !req.body.randS ){
-            res.json({result:0, message:"Lack of parameters"});
-        }else{
-            let key = web3.eth.accounts.recover(req.body.randS,req.body.hashString);
-            res.json({result:1, account:key});
+    app.post("/verifyAccount", function(req, res) {
+        if (!req.body.hashString || !req.body.randS) {
+            res.json({ result: 0, message: "Lack of parameters" });
+        } else {
+            let key = web3.eth.accounts.recover(req.body.randS, req.body.hashString);
+            res.json({ result: 1, account: key });
         }
     });
 
--- server.js
+++ server.js
@@ -7,16 +7,12 @@ app.use("/web3", express.static(__dirname + "/node_modules/web3.js-browser/build
 app.use("/scripts", express.static(__dirname + "/node_modules/web3.js-browser/build/"));
 var fs = require("fs");
 var server = require("http").Server(app);
-server.listen(3000);
+server.listen(3007);
 var io = require("socket.io")(server);
 app.io = io;
 
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 require("./routes/main")(app);
-
-
-
-
-
+console.log("server starting...")
